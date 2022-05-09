3486a8b4e2975a491a32d9c0b32fc9f04f743475
ridwanazeez <33818404+ridwanazeez@users.noreply.github.com>
2022-05-08 21:18:50 -0400

    basic table setup
    after struggling to pass data from the form component to the table component, I moved the form to the main app component and that instantly fixed all my issues without having to type any other code. hilarious.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
-  <Form />
-  <Table />
+  <div class="container">
+    <h1>CSRO Timesheet Parser</h1>
+    <div class="d-flex flex-column justify-content-center align-items-center">
+      <div class="col-4">
+        <form>
+          <div class="mb-3">
+            <label for="fileUpload" class="form-label">Upload JSON File</label>
+            <input type="file" class="form-control" id="fileUpload" @change="handleFile" />
+          </div>
+        </form>
+      </div>
+    </div>
+    <Table :data="output" />
+  </div>
 </template>
 
 <script>
-import Form from "./components/Form.vue";
 import Table from "./components/Table.vue";
 
 export default {
   name: "App",
   components: {
-    Form,
     Table,
+  },
+  data() {
+    return {
+      output: [],
+    };
+  },
+  methods: {
+    handleFile(e) {
+      let files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      this.readFile(files[0]);
+    },
+    readFile(file) {
+      let reader = new FileReader();
+      reader.onload = (e) => {
+        this.output = JSON.parse(e.target.result).Result;
+        // let json = JSON.parse(e.target.result).Result;                //test, ignore
+        // console.log(json);                                            //test output to console log
+      };
+      reader.readAsText(file);
+    },
   },
 };
 </script>
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,7 +1,32 @@
-<template></template>
+<template>
+  <table class="table table-striped table-hover">
+    <thead class="table-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Best Lap</th>
+        <th scope="col">Total Time</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(item, index) in data">
+        <th scope="row">{{ index + 1 }}</th>
+        <td>{{ item.DriverName }}</td>
+        <td>{{ item.BestLap }}</td>
+        <td>{{ item.TotalTime }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
 
 <script>
-export default {};
+export default {
+  props: {
+    data: {
+      type: Array,
+    },
+  },
+};
 </script>
 
 <style></style>
