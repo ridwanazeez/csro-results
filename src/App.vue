08afb6dda204b10ac3ee34550d78250db64bb592
Ridwan Azeez <ridwanazeez2000@gmail.com>
2023-11-15 11:39:23 -0400

    fresh install vue3

--- src/App.vue
+++ src/App.vue
@@ -1,138 +1,47 @@
-<template>
-  <div class="container">
-    <h1>CSRO Timesheet Parser</h1>
-    <div class="d-flex flex-column justify-content-center align-items-center">
-      <div class="col-4">
-        <form id="form">
-          <div class="mb-3">
-            <label for="fileUpload" class="form-label">Upload JSON File</label>
-            <input type="file" class="form-control" id="fileUpload" @change="handleFile" />
-          </div>
-        </form>
-      </div>
-    </div>
-    <Table :data="output" :laps="bestLap" :totaltime="totalTime" />
-  </div>
-</template>
-
-<script>
-import Table from "./components/Table.vue";
-
-export default {
-  name: "App",
-  components: {
-    Table,
-  },
-  data() {
-    return {
-      output: [],
-      bestLap: [],
-      totalTime: [],
-    };
-  },
-  methods: {
-    handleFile(e) {
-      let files = e.target.files || e.dataTransfer.files;
-      if (!files.length) return;
-      this.readFile(files[0]);
-    },
-    calculateBestLap(x) {
-      //Time calculation
-      //Some steps below are a bit repetitive because of how numbers work in JS
-      var a = x / 1000; //milliseconds after decimal point
-      var b = a.toString(); //converts to string
-      var c = parseInt(b); //converted time back to integer, removes decimal without rounding
-      var ms = (c - a) * 1000; //milliseconds, but as a large decimal
-      ms = Math.abs(ms).toFixed(0); //absolute value of milliseconds (round number and removes decimal)
-      var seconds = c % 60; //remainder after dividing by 60 to get seconds of laptime
-      var minutes = (c - seconds) / 60; //subtract seconds from time at C (to avoid decimals) then divided by 60 to get lap minutes
-
-      //console.log(a, b, c, ms, seconds, minutes); //test output
-
-      //Time output
-      if (minutes > 0) {
-        //eg. 1:11:111
-        if (seconds < 10) {
-          //eg. 1:01:111
-          if (ms < 100) {
-            //eg. 1:01:011
-            if (ms < 10) {
-              //eg. 1:01:001
-              return "" + minutes + ":0" + seconds + ":00" + ms; //return 1:01:001
-            } else {
-              return "" + minutes + ":0" + seconds + ":0" + ms; //return 1:01:011
-            }
-          } else {
-            return "" + minutes + ":0" + seconds + ":" + ms; //return 1:01:111
-          }
-        } else {
-          if (ms < 100) {
-            //eg. 1:01:011
-            if (ms < 10) {
-              //eg. 1:01:001
-              return "" + minutes + ":" + seconds + ":00" + ms; //return 1:11:001
-            } else {
-              return "" + minutes + ":" + seconds + ":0" + ms; //return 1:11:011
-            }
-          } else {
-            return "" + minutes + ":" + seconds + ":" + ms; //return 1:11:111
-          }
-        }
-      } else {
-        //eg. 11:111
-        if (seconds < 10) {
-          //eg. 1:01:111
-          if (ms < 100) {
-            //eg. 1:01:011
-            if (ms < 10) {
-              //eg. 1:01:001
-              return "0" + seconds + ":00" + ms; //return 1:01:001
-            } else {
-              return "0" + seconds + ":0" + ms; //return 1:01:011
-            }
-          } else {
-            return "0" + seconds + ":" + ms; //return 1:01:111
-          }
-        } else {
-          if (ms < 100) {
-            //eg. 1:01:011
-            if (ms < 10) {
-              //eg. 1:01:001
-              return seconds + ":00" + ms; //return 1:11:001
-            } else {
-              return seconds + ":0" + ms; //return 1:11:011
-            }
-          } else {
-            return seconds + ":" + ms; //return 1:11:111
-          }
-        }
-      }
-    },
-    readFile(file) {
-      let reader = new FileReader();
-      reader.onload = (e) => {
-        this.output = JSON.parse(e.target.result).Result;
-
-        //extra code that converts the int time to MM:SS:ms
-        let json = JSON.parse(e.target.result).Result;
-        var length = Object.keys(json).length;
-        for (var i = 0; i <= length; i++) {
-          this.bestLap[i] = this.calculateBestLap(json[i].BestLap);
-        }
-      };
-      reader.readAsText(file);
-    },
-  },
-};
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
 }
 </style>
